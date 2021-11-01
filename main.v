1eb85eaee1c55c2fd74ee4734ba54dad15edf2cb
Ahmed Samir <asnweery@gmail.com>
2021-11-01 19:42:54 +0200

    Setup sqlite db

--- main.v
+++ main.v
@@ -1,18 +1,33 @@
 module main
 
 import vweb
-import time
+import sqlite
 
 struct App {
 	vweb.Context
+	pub mut db sqlite.DB
 }
+
 
 fn main() {
-	app := App{}
-	vweb.run(app, 8081)
+	mut app := App{
+		db: sqlite.connect('todos.db') or { panic(err) }
+	}
+
+	sql app.db {
+		create table TodoList
+	}
+
+	first := TodoList{
+		title: 'Do the dishes'
+		desc: 'Get in the kitchen now!!!!!!'
+	}
+
+	sec := TodoList{
+		title: 'Do the laundry'
+		desc: 'Get in the laundry room now!!!!!!'
+	}
+
+	vweb.run(app, 8000)
 }
 
-['/']
-pub fn (mut app App) index() vweb.Result {
-	return app.text(time.now().format())
-}
